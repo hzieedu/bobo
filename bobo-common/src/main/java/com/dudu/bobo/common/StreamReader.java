0f7ebf22b90a4736e6e6dadffe58b55b7ff3f44a
hzieedu <hzieedu@aliyun.com>
2017-03-06 14:06:05 +0800

    添加.gitignore

---
+++ bobo-common/src/main/java/com/dudu/bobo/common/StreamReader.java
@@ -0,0 +1,71 @@
+package com.dudu.bobo.common;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+/**
+ * 
+ * @author liangy43
+ *
+ */
+public class StreamReader {
+	private final SocketChannel	channel;
+
+	private final int	readBufferSize	= 65536;
+	private ByteBuffer	readBuffer		= ByteBuffer.allocate(readBufferSize);
+	private boolean 	corrupted		= false;
+	private int     	msgLen			= 0;
+	
+	public StreamReader(SocketChannel channel) {
+		this.channel = channel;
+	}
+	
+	public Message read() {
+		try {
+			int count = channel.read(readBuffer);
+	        if (count > 0) {
+	        	int len = 0;
+	        	// 确定消息长度
+	        	if (corrupted == false) {
+	        		// 不足4字节?
+	            	if (readBuffer.limit() - readBuffer.position() < 4) {
+	            		return null;
+	            	}
+	            	// 读取消息长度
+	            	len = readBuffer.getInt();	
+	        	} else {
+	        		len = msgLen;
+	        	}
+	        	
+	        	// 如果读入的消息不完整, 则结束该连接的本次读取, 待下次读取
+	        	if (readBuffer.remaining() < len) {
+	        		msgLen = len;
+	        		corrupted = true;
+	        		return null;
+	        	} else {
+	        		msgLen = 0;
+	        		corrupted = false;
+	        	}
+	        	
+	        	// 读取消息
+	        	byte[] bytes = new byte[len];
+	        	readBuffer.get(bytes);
+
+	            // 反序列为对象
+	        	ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+	            ObjectInputStream ois = new ObjectInputStream(bais);
+	            Message message = (Message) ois.readObject();
+				return message;
+	        }
+		} catch (IOException ioex) {
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        return null;
+	}
+}
